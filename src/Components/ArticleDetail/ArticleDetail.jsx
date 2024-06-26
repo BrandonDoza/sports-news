2d22b3c26d246b4a4f74829197645b49c1c034b4
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-26 17:43:51 -0600

    add styling for detail view

--- src/Components/ArticleDetail/ArticleDetail.jsx
+++ src/Components/ArticleDetail/ArticleDetail.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import './ArticleDetail.css'
 
 export default function ArticleDetail ({articles}) {
     const [singleArticle, setSingleArticle] = useState({})
