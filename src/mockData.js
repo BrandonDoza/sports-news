50933dba05c01a0357516978fd9ba843d9aeef39
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-26 13:05:41 -0600

    add mock data

---
+++ src/mockData.js
@@ -0,0 +1,99 @@
+const articles = {
+    "status": "ok",
+    "totalResults": 7,
+    "articles": [
+        {
+            "source": {
+                "id": "google-news",
+                "name": "Google News"
+            },
+            "author": "Bleacher Report",
+            "title": "EA's College Football 25 Rankings: Texas A&M, Alabama Lead 'Toughest Places to Play' - Bleacher Report",
+            "description": null,
+            "url": "https://news.google.com/rss/articles/CBMie2h0dHBzOi8vYmxlYWNoZXJyZXBvcnQuY29tL2FydGljbGVzLzEwMTI1OTQxLWVhcy1jb2xsZWdlLWZvb3RiYWxsLTI1LXJhbmtpbmdzLXRleGFzLWFtLWFsYWJhbWEtbGVhZC10b3VnaGVzdC1wbGFjZXMtdG8tcGxhedIBiwFodHRwczovL3N5bmRpY2F0aW9uLmJsZWFjaGVycmVwb3J0LmNvbS9hbXAvMTAxMjU5NDEtZWFzLWNvbGxlZ2UtZm9vdGJhbGwtMjUtcmFua2luZ3MtdGV4YXMtYW0tYWxhYmFtYS1sZWFkLXRvdWdoZXN0LXBsYWNlcy10by1wbGF5LmFtcC5odG1s?oc=5",
+            "urlToImage": null,
+            "publishedAt": "2024-06-25T15:13:54Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Sports Illustrated"
+            },
+            "author": "Stephen Douglas",
+            "title": "Lakers Looking to Trade First Round Pick, Per Report - Sports Illustrated",
+            "description": "The Lakers plans might include trading No. 17.",
+            "url": "https://www.si.com/nba/lakers-looking-to-trade-first-round-pick-per-report",
+            "urlToImage": "https://images2.minutemediacdn.com/image/upload/c_crop,w_5480,h_3082,x_0,y_0/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/images/ImagnImages/mmsport/si/01j17vtyydy2c4ejrx8p.jpg",
+            "publishedAt": "2024-06-25T15:12:14Z",
+            "content": "The Los Angeles Lakers finally have a new head coach in place so the team's attention can turn to the 2024 NBA draft and the upcoming season. First up they have to navigate the draft, which is the fi… [+1748 chars]"
+        },
+        {
+            "source": {
+                "id": "google-news",
+                "name": "Google News"
+            },
+            "author": "NHL.com",
+            "title": "Avalanche Re-Signs Mittelstadt | Colorado Avalanche - NHL.com",
+            "description": null,
+            "url": "https://news.google.com/rss/articles/CBMiQWh0dHBzOi8vd3d3Lm5obC5jb20vYXZhbGFuY2hlL25ld3MvYXZhbGFuY2hlLXJlLXNpZ25zLW1pdHRlbHN0YWR00gEA?oc=5",
+            "urlToImage": null,
+            "publishedAt": "2024-06-25T15:05:32Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "[Removed]"
+            },
+            "author": null,
+            "title": "[Removed]",
+            "description": "[Removed]",
+            "url": "https://removed.com",
+            "urlToImage": null,
+            "publishedAt": "1970-01-01T00:00:00Z",
+            "content": "[Removed]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Yahoo Entertainment"
+            },
+            "author": "EDDIE PELLS and PAT GRAHAM",
+            "title": "Olympic champion Athing Mu's appeal denied after tumble at US track trials - Yahoo Sports",
+            "description": "Track officials denied an appeal by 800-meter Olympic champion Athing Mu, who got tangled in a pack of runners and fell at the U.S. trials, denying her a...",
+            "url": "https://sports.yahoo.com/olympic-champion-athing-mus-appeal-143857815.html",
+            "urlToImage": "https://s.yimg.com/ny/api/res/1.2/HVDFWRvw50MxDp5Pvyucvw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://media.zenfs.com/en/ap.org/1f542a67860825b8a216188d2742f830",
+            "publishedAt": "2024-06-25T14:38:57Z",
+            "content": "EUGENE, Ore. (AP) Track officials denied an appeal by 800-meter Olympic champion Athing Mu, who got tangled in a pack of runners and fell at the U.S. trials, denying her a chance to defend her title.… [+1239 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Nbcsportsbayarea.com"
+            },
+            "author": "Monte Poole",
+            "title": "Klay, Warriors engaged in tense standoff ahead of NBA free agency - NBC Sports Bay Area",
+            "description": "The Warriors and Klay Thompson are engaged in a tense game of chicken as the franchise icon prepares to hit NBA free agency.",
+            "url": "https://www.nbcsportsbayarea.com/nba/golden-state-warriors/klay-thompson-free-agency-magic-thunder-76ers/1748015/",
+            "urlToImage": "https://media.nbcsportsbayarea.com/2024/06/image-45-2-e1719295341991.png?resize=1200%2C675&quality=85&strip=all",
+            "publishedAt": "2024-06-25T14:17:43Z",
+            "content": "SAN FRANCISCO -- A longtime NBA agent told me years ago, during the Warriors dynastic run, that Golden State CEO Joe Lacob might be the most competitive owner in the league. That he wants to win ever… [+4326 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "247Sports"
+            },
+            "author": "247Sports",
+            "title": "2025 Mock Class 3.0: Post-June official visit edition - 247Sports",
+            "description": null,
+            "url": "https://247sports.com/college/georgia/longformarticle/2025-mock-class-30-post-june-official-visit-edition-233147801/",
+            "urlToImage": null,
+            "publishedAt": "2024-06-25T13:17:35Z",
+            "content": null
+        }
+    ]
+}
+
+export default articles
