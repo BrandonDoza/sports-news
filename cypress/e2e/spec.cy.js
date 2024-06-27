6168d8e7d2b10ab5302cbdaa7752628c5845c922
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-27 11:10:54 -0600

    add testing for search functionality

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -15,5 +15,15 @@ describe('Sports News', () => {
   it('Should be able to click on a article and see detail about it', () => {
     cy.get('.article-cards').first().contains('Connor McDavid').click()
     cy.get('.article-detail').contains('Connor McDavid wins Conn Smythe as playoff MVP despite Oilers losing Stanley Cup Final to Panthers - The Associated Press')
+    cy.get('button').contains('Home').click()
+    cy.get('h1').contains('Sports News')
+  })
+  it('Shoulde be able to search for articles by a keyword', () => {
+    cy.get('input').type('Lakers')
+    cy.get('.article-cards').should('have.length', 1)
+    cy.get('.article-cards').first().contains('Lakers Looking to Trade First Round Pick, Per Report - Sports Illustrated').click()
+    cy.get('.article-detail').contains('The Lakers plans might include trading No. 17.')
+    cy.get('button').contains('Home').click()
+    cy.get('.article-cards').should('have.length', 5)
   })
 })
