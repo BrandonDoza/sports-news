45d99fa366986fee2a4efe7863e1bc99d8b2ae8a
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-27 10:16:28 -0600

    add fetch call to populate data with api data and not mock data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,28 +1,38 @@
 import {Routes, Route} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './App.css';
-import allArticles from '../../mockData';
+// import allArticles from '../../mockData';
 import Header from '../Header/Header';
 import MainPage from '../MainPage/MainPage';
 import ArticleDetail from '../ArticleDetail/ArticleDetail';
+import { fetchArticles } from '../../apiCalls';
 
 function App() {
   const [articles, setArticles] = useState([])
-  function getArticles() {
-    const articlesWithIds = allArticles.articles.map((article, index) => {
-      return {
-        id: index + 1,
-        ...article
-      }
+  function loadArticles() {
+    fetchArticles()
+    .then(data => {
+      console.log(data, 'data')
+      const fullArticlesWithIds = data.articles.filter((element) => {
+        return element.description !== null && element.title !== "[Removed]"
+      })
+      .map((article, index) => {
+        return {
+          id: index + 1,
+          ...article
+        }
+      })
+      setArticles(fullArticlesWithIds)
     })
-    setArticles(articlesWithIds)
+    
+    
   }
 
   useEffect(() => {
-    getArticles()
+    loadArticles()
   },[])
 
-  console.log(articles)
+  console.log(articles, 'articles<><><><><')
 
   return (
     <div className="App">
